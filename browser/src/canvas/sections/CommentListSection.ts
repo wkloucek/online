wkloucek/online

commit f4dc72969689dcfd1fef5d71b138b432e1238707

Mobile: Resolve issue where busy popup covers the whole document
Fixes a bug in the mobile version where the busy popup was erroneously covering the entire document. Adjusted the styling to ensure proper display and user experience.

Signed-off-by: codewithvk <vivek.javiya@collabora.com>
Change-Id: I6b3b9900b3f0891f36aacac3e7ab9013d4e93690

browser/src/control/Control.MobileWizardWindow.js
@@ -40,6 +40,7 @@ L.Control.MobileWizardWindow = L.Control.extend({
 		this.isVisible = false; // indicates if this window is currently visible inside mobile-wizard
 		this.isPopup = false; // indicates that current window is a popup, uses different look
 		this.isSnackBar = false; // shows as little popup at the bottom
+		this.isBusyPopUp = false; // show a busy popup
 		this.isFunctionMenu = false; // shows full screen with list of calc functions
 		this.isHamburgerMenu = false; // shows full screen with items from menubar
 		this.isShapesWizard = false; // shows full screen shape type selector
@@ -79,6 +80,7 @@ L.Control.MobileWizardWindow = L.Control.extend({
 		this.mobileWizard.removeClass('funcwizard');
 		this.mobileWizard.removeClass('popup');
 		this.mobileWizard.removeClass('snackbar');
+		this.mobileWizard.removeClass('busypopup');
 	},
 
 	/// this applies special styling for different types of mobile-wizards
@@ -93,6 +95,8 @@ L.Control.MobileWizardWindow = L.Control.extend({
 			this.mobileWizard.addClass('popup');
 		if (this.isSnackBar)
 			this.mobileWizard.addClass('snackbar');
+		if (this.isBusyPopUp)
+			this.mobileWizard.addClass('busypopup'); 
 	},
 
 	/// resets all classes which can modify the look to the original values
@@ -566,6 +570,10 @@ L.Control.MobileWizardWindow = L.Control.extend({
 					var that = this;
 					this.isSnackBar = true;
 					this.snackBarTimout = setTimeout(function () { that.parent.removeWindow(that); }, data.timeout ? data.timeout : this.options.snackbarTimeout);
+				}
+				else if (data.id === 'busypopup') {
+					var that = this;
+					this.isBusyPopUp = true;
 				}
 			}
 

browser/css/device-mobile.css
@@ -679,15 +679,19 @@ td[id^='tb_spreadsheet-toolbar_item']:focus table.w2ui-button div.w2ui-icon, td[
 }
 
 /* busy popup */
-
-#mobile-wizard.popup:not(.snackbar) {
+#mobile-wizard.popup:not(.snackbar):not(.busypopup) {
 	position: fixed;
 	top: 0;
 	left: 0;
 	height: 100vh !important;
 }
+#mobile-wizard.busypopup {
+	border-radius: 16px 16px 0 0;
+}
 #mobile-wizard.popup #mobile-wizard-content,
-#mobile-wizard.popup .mobile-wizard-content {
+#mobile-wizard.snackbar .mobile-wizard-content,
+#mobile-wizard.busypopup #mobile-wizard-content,
+#mobile-wizard.busypopup .mobile-wizard-content {
 	display: flex;
 	align-items: start;
 	justify-content: center;

commit d47f88d2c09838b81e448a01eeb0ea9995330871

writer: enable @mention for document in mobileview
- also fix fullscreen contentcontrol dropdown and mention
  popup, regression from f4dc72969689dcfd1fef5d71b138b432e1238707

Signed-off-by: Rashesh <rashesh.padia@collabora.com>
Change-Id: Ic2bc0691e95d8a82ec877d36225b9ace882e7e44

browser/css/mobilewizard.css
@@ -1233,20 +1233,24 @@ label.disabled {
 	display: none;
 }
 
-[id$='contentControlModalpopup'].mobile-wizard {
+[id$='contentControlModalpopup'].mobile-wizard, [id$='mentionPopup'].mobile-wizard {
 	display: flex;
 	flex: 1;
 	align-self: stretch;
 }
 
-#contentControlList.mobile-wizard {
+#contentControlList.mobile-wizard, #mentionPopupList.mobile-wizard {
 	margin: 0;
 	border-color: transparent;
 	width: 100%;
 }
 
-#contentControlList.mobile-wizard li:not(:last-child) {
+#contentControlList.mobile-wizard li:not(:last-child), #mentionPopupList.mobile-wizard li:not(:last-child) {
 	border-bottom: 1px solid var(--color-border-lighter);
+}
+
+#contentControlList.mobile-wizard .ui-treeview-body, #mentionPopupList.mobile-wizard .ui-treeview-body {
+	max-height: none;
 }
 
 .unotoolbutton.mobile-wizard .unobutton img{

browser/src/canvas/sections/CommentListSection.ts
@@ -470,6 +470,7 @@ export class CommentSection extends app.definitions.canvasSectionObject {
 				layoutstyle: 'end'
 			},
 		]);
+		json.isPopupFullscreen = true;
 
 		var cancelFunction = function() {
 			this.cancel(comment);

browser/src/control/AutoCompletePopup.ts
@@ -32,6 +32,7 @@ abstract class AutoCompletePopup {
 	protected newPopupData: PopupData;
 	protected data: MessageEvent<any>;
 	protected popupId: string;
+	protected isMobile: boolean;
 
 	constructor(popupId: string, map: ReturnType<typeof L.map>) {
 		this.map = map;
@@ -53,6 +54,8 @@ abstract class AutoCompletePopup {
 			clickToClose: '_POPOVER_',
 			id: this.popupId,
 		} as PopupData;
+
+		this.isMobile = (<any>window).mode.isMobile();
 
 		this.onAdd();
 
@@ -112,7 +115,8 @@ abstract class AutoCompletePopup {
 	}
 
 	sendJSON(data: any): void {
-		this.map.fire('jsdialog', {
+		const fireEvent = this.isMobile ? 'mobilewizard' : 'jsdialog';
+		this.map.fire(fireEvent, {
 			data: data,
 			callback: this.callback.bind(this),
 		});

browser/src/control/Control.MobileWizardWindow.js
@@ -93,7 +93,7 @@ L.Control.MobileWizardWindow = L.Control.extend({
 			this.mobileWizard.addClass('menuwizard');
 		if (this.isFunctionMenu)
 			this.mobileWizard.addClass('funcwizard');
-		if (this.isPopup)
+		if (this.isPopup && this.isPopupFullscreen)
 			this.mobileWizard.addClass('popup');
 		if (this.isSnackBar)
 			this.mobileWizard.addClass('snackbar');
@@ -523,6 +523,7 @@ L.Control.MobileWizardWindow = L.Control.extend({
 
 			this._reset();
 			this.isPopup = isPopupJson;
+			this.isPopupFullscreen = data.isPopupFullscreen;
 
 			this._showWizard();
 			if (this.map._docLayer && !this.map._docLayer.isCalc()) {

browser/src/control/Control.UIManager.js
@@ -252,7 +252,6 @@ L.Control.UIManager = L.Control.extend({
 
 			this.map.sidebar = JSDialog.Sidebar(this.map, {animSpeed: 200});
 
-			this.map.mention = L.control.mention(this.map);
 			this.map.formulaautocomplete = L.control.formulaautocomplete(this.map);
 			this.map.formulausage = L.control.formulausage(this.map);
 		}
@@ -381,6 +380,8 @@ L.Control.UIManager = L.Control.extend({
 			var showResolved = this.getBooleanDocTypePref('ShowResolved', true);
 			if (showResolved === false || showResolved === 'false')
 				this.map.sendUnoCommand('.uno:ShowResolvedAnnotations');
+
+			this.map.mention = L.control.mention(this.map);
 		}
 
 		if (this.map.isPresentationOrDrawing() && (isDesktop || window.mode.isTablet())) {